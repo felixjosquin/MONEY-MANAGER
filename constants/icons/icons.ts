9c28d3982c5ecdc98ce159d763b139b7035ebb47
felixjosquin <fj.josquin@gmail.com>
2025-11-30 00:19:57 +0100

    add new icons

--- constants/icons/icons.ts
+++ constants/icons/icons.ts
@@ -6,14 +6,20 @@ import CreditCard2BackFill from "@/assets/icons/credit-card-2-back-fill.svg";
 import EuroCircle from "@/assets/icons/euro-circle.svg";
 import PiggyBankSolid from "@/assets/icons/piggy-bank-solid.svg";
 import Question from "@/assets/icons/bx-question-mark.svg";
+import People from "@/assets/icons/people.svg";
+import Apple from "@/assets/icons/apple.svg";
+import Fork from "@/assets/icons/fork.svg";
 
 export const ICONS = {
-  "bank-outline": BankOutline,
-  "bx-dollar-circle": BxDollarCircle,
-  "bxs-credit-card-alt": BxsCreditCardAlt,
-  "cash-multiple": CashMultiple,
-  "credit-card-2-back-fill": CreditCard2BackFill,
-  "euro-circle": EuroCircle,
-  "piggy-bank-solid": PiggyBankSolid,
+  bank: BankOutline,
+  dollar: BxDollarCircle,
+  "credit-card": BxsCreditCardAlt,
+  cash: CashMultiple,
+  "credit-card-2": CreditCard2BackFill,
+  euro: EuroCircle,
+  "piggy-bank": PiggyBankSolid,
   question: Question,
+  people: People,
+  fork: Fork,
+  apple: Apple,
 };
