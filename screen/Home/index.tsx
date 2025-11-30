c6c45f16f7186513738c12f766119f242b02a05d
felixjosquin <fj.josquin@gmail.com>
2025-11-30 02:36:12 +0100

    update layoout

--- screen/Home/index.tsx
+++ screen/Home/index.tsx
@@ -2,13 +2,20 @@ import { AccountSelector } from "@/components/AccountSelector";
 import AccountService from "@/db/services/account/AccountService";
 import { useDbQuery } from "@/hooks/useDbQuery";
 import { MainLayout } from "../MainLayout";
+import { useEffect } from "react";
+import CategoryService from "@/db/services/category/CategoryService";
+import { useSQLiteContext } from "expo-sqlite";
 
 export function Home() {
   const { data: accounts } = useDbQuery({
     queryFn: AccountService.getAccounts,
     queryKey: ["accounts"],
   });
+  const db = useSQLiteContext();
 
+  useEffect(() => {
+    CategoryService.getCategoryWithTotal(db);
+  }, [db]);
   return (
     <MainLayout>
       <AccountSelector accounts={accounts} />
